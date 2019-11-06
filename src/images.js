3d2c11fe8b506382420ea9c93c141abe6ab70911
erika gonzalez <gonzalezerika75951@gmail.com>
2019-11-05 22:43:33 -0600

    work uploaded

---
+++ src/images.js
@@ -0,0 +1,29 @@
+import beth from './images/beavers.jpg';
+import birdperson from './images/chipmunk.jpg';
+import evilmorty from './images/giraffe.jpg';
+import gianthead from './images/hedgehog.jpg';
+import goldenford from './images/kitten.jpg';
+import jerry from './images/koala.jpg';
+import jessica from './images/lion.jpg';
+import meeseeks from './images/pig.jpg';
+import morty from './images/redpanda.jpg';
+import mr from './images/seal.jpg';
+import rick from './images/pug.jpg';
+import summer from './images/dog.jpg';
+
+const images = [
+  beth,
+  birdperson,
+  evilmorty,
+  gianthead,
+  goldenford,
+  jerry,
+  jessica,
+  meeseeks,
+  morty,
+  mr,
+  rick,
+  summer
+];
+
+export default images;
